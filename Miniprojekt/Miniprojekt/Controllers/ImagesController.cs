f9d5bd6c1bada047675db70858c8dd45abd36fa9
Phixle <henry.ohman@yahoo.se>
2016-12-13 16:07:54 +0100

    Update

---
+++ Miniprojekt/Miniprojekt/Controllers/ImagesController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Miniprojekt.DataAccess;
+using Miniprojekt.Models.ImgText;
+using Miniprojekt.Repository;
+
+namespace Miniprojekt.Controllers
+{
+    public class ImagesController : Controller
+    {
+        private ImgTextRepository repo = new ImgTextRepository();
+
+        // GET: Images
+        public ActionResult Index()
+        {
+            return View(repo.db.Images.ToList());
+        }
+
+        // GET: Images/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Image image = repo.db.Images.Find(id);
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+            return View(image);
+        }
+
+        // GET: Images/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Images/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Url,Desciption")] Image image)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.db.Images.Add(image);
+                repo.db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(image);
+        }
+
+        // GET: Images/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Image image = repo.db.Images.Find(id);
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+            return View(image);
+        }
+
+        // POST: Images/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Url,Desciption")] Image image)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.db.Entry(image).State = EntityState.Modified;
+                repo.db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(image);
+        }
+
+        // GET: Images/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Image image = repo.db.Images.Find(id);
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+            return View(image);
+        }
+
+        // POST: Images/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Image image = repo.db.Images.Find(id);
+            repo.db.Images.Remove(image);
+            repo.db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                repo.db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Miniprojekt/Miniprojekt/Repository/ImgTextRepository.cs
@@ -0,0 +1,18 @@
+﻿using Miniprojekt.DataAccess;
+using Miniprojekt.Models.ImgText;
+
+namespace Miniprojekt.Repository
+{
+    public class ImgTextRepository
+    {
+        public ImgTextContext db = new ImgTextContext();
+
+        public bool CompareTextImg(string text, Image img)
+        {
+            if (img.Desciption == text)
+                return true;
+            else
+                return false;
+        }
+    }
+}
