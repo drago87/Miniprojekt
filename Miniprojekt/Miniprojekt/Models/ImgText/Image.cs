f92c699d51702dce93a89995620b215da0cf81fe
Phixle <henry.ohman@yahoo.se>
2016-12-13 10:38:30 +0100

    Text-Bild modell: Image.cs

---
+++ Miniprojekt/Miniprojekt/Models/ImgText/Image.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Miniprojekt.Models.ImgText
+{
+    public class Image
+    {
+        [Key]
+        public int Id { get; protected set; }
+
+        public string Url { get; protected set; }
+
+        public string Desciption { get; protected set; }
+
+        public Image() { }
+
+        public Image(string url, string description)
+        {
+            this.Url = url;
+            this.Desciption = description;
+        }
+
+        public void SetUrl(string url)
+        {
+            this.Url = url;
+        }
+
+        public void SetDescription(string description)
+        {
+            this.Desciption = description;
+        }
+    }
+}
