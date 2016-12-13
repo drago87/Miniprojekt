dba193925bf215ef0a98a3057726e1294beac658
Emil Eklund <emileklund@outlook.com>
2016-12-13 10:24:15 +0100

    Added Basic Text2Color Related Items

---
+++ Miniprojekt/Miniprojekt/Models/Text2Color/Score.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Miniprojekt.Models.Text2Color
+{
+    public class Highscore
+    {
+        public string Username { get; protected set; }
+        public int Points { get; protected set; }
+        public DateTime Time { get; protected set; }
+
+        public Highscore() { }
+
+        public Highscore(string username, int points, DateTime time)
+        {
+            this.Username = username;
+            this.Points = points;
+            this.Time = time;
+        }
+    }
+}
