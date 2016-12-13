7518521d63da4a455738380bf84d3c3ce871a513
Emil Eklund <emileklund@outlook.com>
2016-12-13 15:57:30 +0100

    Creating Game

--- Miniprojekt/Miniprojekt/Migrations/Text2Color/Configuration.cs
+++ Miniprojekt/Miniprojekt/Migrations/Text2Color/Configuration.cs
@@ -16,11 +16,29 @@ namespace Miniprojekt.Migrations.Text2Color
 
         protected override void Seed(Miniprojekt.DataAccess.Text2ColorContext context)
         {
+            // Update-Database -ConfigurationTypeName Miniprojekt.Migrations.Text2Color.Configuration
             context.Colors.AddOrUpdate(
                 p => p.Name,
-                new Color("Green", "#00FF00"),
-                new Color("Red", "#FF0000"),
-                new Color("Blue", "#0000FF")
+                new Color("AliceBlue", "#F0F8FF"),
+                new Color("AntiqueWhite", "#FAEBD7"),
+                new Color("Aqua", "#00FFFF"),
+                new Color("Aquamarine", "#7FFFD4"),
+                new Color("Azure", "#F0FFFF"),
+                new Color("Beige", "#F5F5DC"),
+                new Color("Bisque", "#FFE4C4"),
+                new Color("BlanchedAlmond", "#FFEBCD"),
+                new Color("HoneyDew", "#F0FFF0"),
+                new Color("MintCream", "#F5FFFA"),
+                new Color("GhostWhite", "#F8F8FF"),
+                new Color("LightGoldenRodYellow", "#FAFAD2"),
+                new Color("LemonChiffon", "#FFFACD"),
+                new Color("FloralWhite", "#FFFAF0"),
+                new Color("Snow", "#FFFAFA"),
+                new Color("LightYellow", "#FFFFE0"),
+                new Color("Ivory", "#FFFFF0"),
+                new Color("White", "#FFFFFF"),
+                new Color("SlateGray", "##708090"),
+                new Color("LightSlateGray", "#778899")
             );
         }
     }
