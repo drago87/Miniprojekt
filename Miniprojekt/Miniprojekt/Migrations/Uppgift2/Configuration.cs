8ce8e9be37b5e07e4c8e05e70ac92c66b096cc4f
drago87 <dr.drago.87@hotmail.com>
2016-12-16 12:05:20 +0100

    Added standard Sentences

--- Miniprojekt/Miniprojekt/Migrations/Uppgift2/Configuration.cs
+++ Miniprojekt/Miniprojekt/Migrations/Uppgift2/Configuration.cs
@@ -4,6 +4,7 @@ namespace Miniprojekt.Migrations.Uppgift2
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Miniprojekt.Models.Uppgift2Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Miniprojekt.DataAccess.DataAccessLayerUppgift2>
     {
@@ -15,6 +16,12 @@ namespace Miniprojekt.Migrations.Uppgift2
 
         protected override void Seed(Miniprojekt.DataAccess.DataAccessLayerUppgift2 context)
         {
+            context.uppgift2.AddOrUpdate(
+                p => p.ID,
+                new Uppgift2 { Mening = "Sa du något?" },
+                new Uppgift2 { Mening = "Bra något nytt." },
+                new Uppgift2 { Mening = "Det var en stor Groda!" });
+
             // Update-Database -ConfigurationTypeName Miniprojekt.Migrations.Uppgift2.Configuration
 
             //  This method will be called after migrating to the latest version.
