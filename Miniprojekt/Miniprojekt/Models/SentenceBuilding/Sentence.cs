5406a33b864fddfddf1b7cbae9f84f75da9f0cb9
Emil Eklund <emileklund@outlook.com>
2016-12-16 13:20:38 +0100

    Stilsättning

--- Miniprojekt/Miniprojekt/Models/SentenceBuilding/Sentence.cs
+++ Miniprojekt/Miniprojekt/Models/SentenceBuilding/Sentence.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,7 @@ namespace Miniprojekt.Models.SentenceBuilding
     {
         [Key]
         public int ID { get; set; }
-        [Required]
+        [Required, DisplayName("Tillgängliga Ord")]
         public string mySentence { get; set; }
     }
 }
