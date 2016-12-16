eb5694a7194ccbc7e27d1a2963063817eafdd5d8
Marcus <marcus_bok@msn.com>
2016-12-16 11:38:58 +0100

    Snetences Vy

---
+++ Miniprojekt/Miniprojekt/Repositories/SentenceBuilderRepository.cs
@@ -0,0 +1,62 @@
+﻿using Miniprojekt.DataAccess;
+using Miniprojekt.Models.SentenceBuilding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Miniprojekt.Repositories
+{
+    public class SentenceBuilderRepository
+    {
+        SBContext Database = new SBContext();
+
+        public List<Sentence> GetAllSentences()
+        {
+            return Database.Sentences.ToList();
+        }
+
+        public Sentence GetRandomSentence()
+        {
+            List<Sentence> sentences = Database.Sentences.ToList();
+            Random rand = new Random();
+            int random = rand.Next(sentences.Count);
+            Sentence sent = sentences[random];
+            List<string> temp = sent.mySentence.Split(' ').ToList();
+            string sentence = "";
+            int length = temp.Count;
+            for (int i = 0; i < length; i++)
+            {
+                string word = temp[rand.Next(temp.Count)];
+                sentence += " " + word;
+                temp.Remove(word);
+            }
+            return new Sentence {mySentence = sentence };
+
+        }
+
+        public ExamResult Examine(string StudentAnswers, string rightAnswers)
+        {
+
+            return Exam.Evaluate(StudentAnswers.Split(' ').Where(x => x != "").ToList(), rightAnswers.Split(' ').Where(x => x != "").ToList());
+
+            //return Exam.Evaluate(((Func<List<string>>)(() =>{
+            //    List<string> student = new List<string>();
+            //    foreach (string word in StudentAnswers.Split(' '))
+            //    {
+            //        student.Add(word);
+            //    }
+            //    return student;
+            //}))(), ((Func<List<string>>)(() =>
+            //{
+            //    List<string> right = new List<string>();
+            //    foreach (string word in rightAnswers.Split(' '))
+            //    {
+            //        right.Add(word);
+            //    }
+            //    return right;
+            //}))());
+        }
+
+    }
+}
