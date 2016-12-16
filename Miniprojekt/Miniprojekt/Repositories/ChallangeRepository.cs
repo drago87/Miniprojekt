7143e86d09e0eb3d95118b20f5403d1ac59dfdb9
drago87 <dr.drago.87@hotmail.com>
2016-12-16 13:44:49 +0100

    Not done will problebly give errors

---
+++ Miniprojekt/Miniprojekt/Repositories/ChallangeRepository.cs
@@ -0,0 +1,24 @@
+﻿using Miniprojekt.Models.Challange;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Miniprojekt.Repositories
+{
+    public class ChallangeRepository
+    {
+        private ChallangeContext db;
+
+
+        public ChallangeRepository()
+        {
+            db = new ChallangeContext();
+        }
+
+        public ICollection<Question> GetAllQuestions
+        {
+
+        }
+    }
+}
